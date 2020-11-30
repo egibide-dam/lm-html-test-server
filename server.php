8206896b6e424de17c9af0b24d4e009d11ac5eb5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-12-01 00:34:17 +0100

    Diseño mejorado

--- server.php
+++ server.php
@@ -5,52 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
           integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Visualizador de GET y POST</title>
+    <title>Visualizador de peticiones HTTP</title>
     <style>
-        .footer {
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-            height: 60px;
-            line-height: 60px;
-            background-color: #eeeeee;
+        html,
+        body {
+            height: 100%;
+        }
+
+        #page-content {
+            flex: 1 0 auto;
+        }
+
+        #sticky-footer {
+            flex-shrink: 0;
         }
     </style>
 </head>
-<body>
+<body class="d-flex flex-column">
 
-<main role="main" class="container">
-    <h1 class="mt-3 text-bold">Visualizador de GET y POST</h1>
-    <hr>
-    <div class="row mt-3">
-        <div class="col-12 my-auto">
-            <h2>$_GET</h2>
+<div id="page-content">
+    <main class="container">
+        <h1 class="mt-3 display-4">Visualizador de peticiones HTTP</h1>
+        <hr>
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <h5 class="card-header">GET</h5>
+                    <div class="card-body">
+                        <pre class="text-primary m-0 p-0"><?php echo(!empty($_GET) ? print_r($_GET, true) : 'No hay datos.') ?></pre>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-12 py-3 my-1 bg-dark rounded">
-            <pre class="text-light m-0 p-0"><?php print_r($_GET); ?></pre>
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <h5 class="card-header">POST</h5>
+                    <div class="card-body">
+                        <pre class="text-primary m-0 p-0"><?php echo(!empty($_POST) ? print_r($_POST, true) : 'No hay datos.') ?></pre>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row mt-3">
-        <div class="col-12 my-auto">
-            <h2>$_POST</h2>
-        </div>
-        <div class="col-12 py-3 my-1 bg-dark rounded">
-            <pre class="text-light m-0 p-0"><?php print_r($_POST); ?></pre>
-        </div>
-    </div>
-    <div>
-    </div>
-</main>
+    </main>
+</div>
 
-<footer class="footer">
-    <div class="container">
+<!-- REF: https://startbootstrap.com/snippets/sticky-footer-flexbox -->
+<footer id="sticky-footer" class="mt-5 py-4 bg-light">
+    <div class="container text-center">
         <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
             <img alt="Licencia de Creative Commons" style="border-width:0"
                  src="https://i.creativecommons.org/l/by/4.0/88x31.png"/>
         </a>
-        <span class="text-muted ml-2">Esta web está bajo <a rel="license"
-                                                            target="_blank"
-                                                            href="http://creativecommons.org/licenses/by/4.0/">licencia CC BY 4.0</a>.</span>
+        <small class="text-muted ml-2">Esta web está bajo <a rel="license"
+                                                             target="_blank"
+                                                             href="http://creativecommons.org/licenses/by/4.0/">licencia
+                CC BY 4.0</a>.</small>
     </div>
 </footer>
 
